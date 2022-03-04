d3c8c87091d7971595c044ea473ee1b7c49f7db2
Newon-universe <chokovon@gmail.com>
2022-03-04 23:21:43 +0900

    feat : BottomBar, TopBar, MainContent Card 구현

---
+++ app/src/main/java/com/example/sdm_eco_mileage/navigation/SecomiScreens.kt
@@ -0,0 +1,17 @@
+package com.example.sdm_eco_mileage.navigation
+
+enum class SecomiScreens {
+    SplashScreen,
+    LoginScreen,
+    RegisterScreen,
+    FindingAccountScreen,
+    HomeScreen,
+    HomeDetailScreen,
+    HomeAddScreen,
+    SearchScreen,
+    EducationScreen,
+    EventScreen,
+    MyPageScreen,
+    RankingScreen,
+    SettingsScreen
+}
