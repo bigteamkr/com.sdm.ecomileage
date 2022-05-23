1ad306e5ac4078dc0dd9ee43a0a6212e78d1a12d
Newon-universe <chokovon@gmail.com>
2022-05-23 18:26:38 +0900

    fix : event ui update

--- app/src/main/java/com/sdm/ecomileage/screens/event/EventScreen.kt
+++ app/src/main/java/com/sdm/ecomileage/screens/event/EventScreen.kt
@@ -3,29 +3,33 @@ package com.sdm.ecomileage.screens.event
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.pagerTabIndicatorOffset
 import com.google.accompanist.pager.rememberPagerState
 import com.google.accompanist.systemuicontroller.SystemUiController
+import com.sdm.ecomileage.R
 import com.sdm.ecomileage.components.SecomiBottomBar
 import com.sdm.ecomileage.components.SecomiMainFloatingActionButton
 import com.sdm.ecomileage.navigation.SecomiScreens
-import com.sdm.ecomileage.ui.theme.SelectedTabColor
-import com.sdm.ecomileage.ui.theme.UnSelectedTabColor
+import com.sdm.ecomileage.ui.theme.*
 import kotlinx.coroutines.launch
 
 
@@ -108,44 +112,173 @@ private fun EventAttendScaffold(navController: NavController) {
         mutableStateOf(true)
     }
     val pagerState = rememberPagerState()
+    val configuration = LocalConfiguration.current
 
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) {
-
-        HorizontalPager(
-            count = 1,
-            state = pagerState,
-            modifier = Modifier.fillMaxSize()
-        ) { page ->
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Image(
-                painter = painterResource(id = com.sdm.ecomileage.R.drawable.image_event_sample),
-                contentDescription = "",
-                contentScale = ContentScale.Fit,
+                painter = painterResource(id = R.drawable.image_attendance_check),
+                contentDescription = "출석체크 이미지",
                 modifier = Modifier
-                    .padding(vertical = 60.dp)
-                    .fillMaxSize()
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f),
+                contentScale = ContentScale.FillWidth
             )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp, vertical = 10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "2022.05.23", fontWeight = FontWeight.Bold)
+                Text(text = "2022.05.29", fontWeight = FontWeight.Bold)
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .padding(top = 5.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                repeat(4) {
+                    Surface(
+                        modifier = Modifier.size(70.dp),
+                        shape = CircleShape,
+                        color = BottomUnSelectedColor,
+                        contentColor = Color.White
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "출석",
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 17.sp
+                            )
+                        }
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .padding(top = 5.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                repeat(3) {
+                    Surface(
+                        modifier = Modifier.size(70.dp),
+                        shape = CircleShape,
+                        color = BottomUnSelectedColor,
+                        contentColor = Color.White
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "출석",
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 17.sp
+                            )
+                        }
+                    }
+                }
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .width((configuration.screenWidthDp * 0.9).dp)
+                    .height(70.dp)
+                    .padding(top = 10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = LoginButtonColor,
+                    contentColor = Color.White
+                )
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "출석 도장 찍기", fontWeight = FontWeight.SemiBold)
+                    Text(text = "(일일 1회 출석)", fontWeight = FontWeight.SemiBold)
+                }
+            }
         }
-
     }
 }
 
 @Composable
 private fun QuizChallengeScaffold(navController: NavController) {
+    val configuration = LocalConfiguration.current
+    var inputValue by remember {
+        mutableStateOf("")
+    }
+
     Scaffold {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.White),
-            verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                painterResource(id = com.sdm.ecomileage.R.drawable.image_quiz_sample),
+                painterResource(id = R.drawable.image_quiz),
                 contentDescription = "Sample",
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.padding(vertical = 60.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f),
+                contentScale = ContentScale.FillWidth
+            )
+            Card(
+                shape = RoundedCornerShape(10f),
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .height(100.dp)
+                    .padding(top = 10.dp),
+            ) {
+                BasicTextField(value = inputValue, onValueChange = { inputValue = it }) {
+                    Box(modifier = Modifier.padding(top = 10.dp, start = 10.dp)) {
+                        if (inputValue == "")
+                            Text(
+                                text = "100자 이내로 정답을 작성해주세요.",
+                                color = PlaceholderColor
+                            )
+                        it()
+                    }
+                }
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .height(50.dp)
+                    .padding(top = 10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = LoginButtonColor,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "제출")
+            }
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                color = UnselectedButtonColor,
+                thickness = 1.dp
             )
         }
     }
