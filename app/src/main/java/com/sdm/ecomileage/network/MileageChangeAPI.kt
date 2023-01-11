e1030c238321d9b248d935022a1f7431b52c0879
BigTeam Inc <91238440+bigteamkr@users.noreply.github.com>
2023-01-11 20:42:01 +0900

    mileage changi api 1st commit

--- app/src/main/java/com/sdm/ecomileage/network/MileageChangeAPI.kt
+++ app/src/main/java/com/sdm/ecomileage/network/MileageChangeAPI.kt
@@ -1,16 +1,21 @@
 package com.sdm.ecomileage.network
 
+import com.google.android.play.core.install.model.InstallErrorCode
 import com.sdm.ecomileage.model.mileagechange.request.MileageChangeRequest
 import com.sdm.ecomileage.model.mileagechange.response.MileageChangeResponse
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
-
+import javax.inject.Singleton
 
 interface MileageChangeAPI {
+    // 마일리지 전환신청
     @POST("AppConvertMileageInfo")
-    // @POST("MileageChangeInfo")
-    suspend fun postMileageChangeInfo(
+    suspend fun postMileageChangeAPI(
         @Header ("token") token:String,
         @Body body : MileageChangeRequest
     ) : MileageChangeResponse
