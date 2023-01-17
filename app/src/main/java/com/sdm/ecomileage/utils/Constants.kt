1bf349b9e4e594ffee6eade8a4eec88c1b492ced
BigTeam Inc <91238440+bigteamkr@users.noreply.github.com>
2023-01-17 19:14:03 +0900

    operation url

--- app/src/main/java/com/sdm/ecomileage/utils/Constants.kt
+++ app/src/main/java/com/sdm/ecomileage/utils/Constants.kt
@@ -5,5 +5,6 @@ import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.sdm.ecomileage.SdmEcoMileageApplication
 
 object Constants {
+    // 운영
     const val BASE_URL = "http://api.ecosdm.com/app/"
 }
